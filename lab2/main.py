b81dae102fecdd31a6570a8c39c03639c6303116
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2017-03-14 19:30:14 +0300

    Добавил комментарии

--- lab2/main.py
+++ lab2/main.py
@@ -7,12 +7,10 @@ from skimage.measure import label, regionprops
 from skimage.color import label2rgb
 
 def getDishFromPicture(pictureName, r1, r2, g1, g2, b1, b2, regionSize, dishName):
+    # Загрузка цифрового изображения
     image1 = io.imread(pictureName)
 
-    red_channel = image1[:, :, 0]
-    green_channel = image1[:, :, 1]
-    blue_channel = image1[:, :, 2]
-
+    # Фильтрация по RGB
     mask_r_1 = image1[...,0] > r1
     mask_r_2 = image1[...,0] < r2
     mask_g_1 = image1[...,1] > g1
@@ -20,6 +18,7 @@ def getDishFromPicture(pictureName, r1, r2, g1, g2, b1, b2, regionSize, dishName
     mask_b_1 = image1[...,2] > b1
     mask_b_2 = image1[...,2] < b2
 
+    # Получили картинку где "горят" нужные пиксели
     image_masked = mask_r_1 & mask_r_2 & mask_g_1 & mask_g_2 & mask_b_1 & mask_b_2
 
     label_image = label(image_masked.astype(bool))
@@ -29,9 +28,6 @@ def getDishFromPicture(pictureName, r1, r2, g1, g2, b1, b2, regionSize, dishName
     # Создает дополнительную фигуру
     fig, ax = plt.subplots(figsize=(10, 6))
     ax.imshow(image1)
-
-    coord = {}
-    i = 1
 
     # В цикде проходтимся по всем местам, где есть отметки
     for region in regionprops(label_image):
@@ -52,14 +48,14 @@ def getDishFromPicture(pictureName, r1, r2, g1, g2, b1, b2, regionSize, dishName
             ax.plot(x0, y0, 'rs', markersize=10)
             ax.text(x0, y0, dishName, fontsize=12)
 
-            #print("[%s] x0: %s y0: %s\n" % (i, x0, y0,))
-            i += 1
-
     ax.set_axis_off()
     plt.tight_layout()
     plt.show()
 
 def getDishesFromPicture(pictureName):
+
+    #Различные блюда
+
     getDishFromPicture(pictureName, 140, 190, 60, 110, 30, 55, 500, "Салат с морковью")
     getDishFromPicture(pictureName, 76, 90, 57, 71, 36, 59, 200, "Черный хлеб")
     getDishFromPicture(pictureName, 140, 188, 125, 180, 82, 143, 2200, "Пюре картофельное")
